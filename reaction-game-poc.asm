27f14920effda9c29831fba25393cfaaedef0e84
I749983 <lkronenb1@googlemail.com>
2026-05-19 12:16:50 +0200

    feat: Game working ig

--- reaction-game-poc.asm
+++ reaction-game-poc.asm
@@ -14,42 +14,47 @@
 ; Start = P1.0
 ; Stop  = P1.1
 ; Reset = P1.2
+;
+; P0.0 = Merker RESET
+; P0.1 = Lampe (1=aus, 0=an)
+; 30h  = Vorcountdown (10 Sek)
 ; -------------------------------------------------
-cseg at 0h
-ajmp init
-cseg at 100h
-
-; ------------------------------------------------
-; Interrupt für TIMER0: Einsprung bei 0Bh
-;-------------------------------------------------
-ORG 0Bh
+cseg at 0h ; Alles was jetzt kommt an Adresse 0 im Speicher
+ajmp init ; Das hier wird direkt ausgeführt
+cseg at 0Bh ; Wenn Timer abläuft dann führ das aus
 call timer
-reti
+reti ; Ende des Interrupts
 ;-------------------------------------------------------
-;init: TIMER wird initialisiert
-; für 40 ms benötigt man einen 16 bit Timer
-; das dauert zu lange in der Simulation! 
-; Daher hier eine kurze Variante!
-; Es wird nur von C0h auf FFh hochgezählt und 
-; dann der Timer wird auf C0h gesetzt
-; (für Hardware müsste das ersetzt werden!)
+; init
 ;-------------------------------------------------------
-ORG 20h
+ORG 20h ; freie Adresse
 init:
 mov IE, #10010010b
 mov tmod, #00000010b
-mov r7, #00h ; Minuten
-mov r6, #00h ; Sekunden
-mov tl0, #0c0h  ; Timer-Initionalsierung 
+mov r7, #00h        ; Minuten
+mov r6, #00h        ; Sekunden
+mov r1, #00h        ; Interrupt-Zähler
+mov tl0, #0c0h
 mov th0, #0c0h
-mov P1,#00h
-setb P0.0 ; Merker für RESET
-;-----------------------------------------------------------------------
-; die Voreingestellten Minuten und Sekunden erscheinen auf dem Display
-;-----------------------------------------------------------------------
+mov P1, #00h
+setb P0.0           ; Merker für RESET
+setb P0.1           ; Lampe aus
+mov 30h, #64h       ; 10 Sekunden Vorcountdown
+call zeigen
+initGame:
+mov IE, #10010010b
+mov tmod, #00000010b
+mov r1, #00h        ; Interrupt-Zähler
+mov tl0, #0c0h
+mov th0, #0c0h
+mov P1, #00h
+setb P0.0           ; Merker für RESET
+setb P0.1           ; Lampe aus
+mov 30h, #64h       ; 10 Sekunden Vorcountdown
 call zeigen
 ;---------------------------
 anfang:
+clr P0.0
 jnb p1.0, starttimer
 jnb p1.1, stoptimer
 nurRT:
@@ -57,48 +62,53 @@ jnb p1.2, RT
 jnb tr0, da
 ajmp anfang
 da:
+jb P0.1, anfang     ; Lampe aus = Vorcountdown läuft, kein Display
 call display
-jnb P0.0, nurRT
 ajmp anfang
-;------------------------------
-; Hauptprogrammschleife
-;
-; Start = P1.0
-; Stop  = P1.1
-; Reset = P1.2
 ;------------------------------
 starttimer:
-setb tr0; start timer0
+setb tr0
 setb P1.0
 ajmp anfang
-; stop Timer
 stoptimer:
-clr tr0; stop timer
+clr tr0
 setb P1.1
 ajmp anfang
-; reset Timer
 RT:
-clr tr0; stop timer
+clr tr0
 setb P1.2
-ljmp init
+ljmp initGame
 ;---------------------------------------------
-; timer
-; Zählt 1 Sekunde: 25 mal 40 Millisekunden
-; 24mal wird nur die Anzeige "refresht"
-; beim 25mal wird die Zeit runter gezählt
-;(hier: nur 2mal und nur wenige my Sekunden)
+; timer ISR
 ;---------------------------------------------
 timer:
+mov a, 30h
+jnz vorcountdown
+; Vorcountdown fertig, normale Stoppuhr
 inc r1
 cjne r1, #02h, nuranzeige
 mov r1, #00h
 call countup
 ret
-
 nuranzeige:
 call display
 ret
-
+;---------------------------------------------
+; vorcountdown: 100-Sek still runterzaehlen
+;---------------------------------------------
+vorcountdown:
+inc r1
+cjne r1, #02h, cdwait
+mov r1, #00h
+dec 30h
+mov a, 30h
+jnz cdwait
+clr P0.1            ; Lampe AN
+cdwait:
+ret
+;---------------------------------------------
+; countup: Stoppuhr hochzaehlen
+;---------------------------------------------
 countup:
 inc r6
 cjne r6, #3ch, sekunden
@@ -106,28 +116,16 @@ mov r6, #00h
 inc r7
 cjne r7, #63h, minuten
 hupe:
-clr tr0; stop timer
-clr P0.0
+clr tr0
 ret
-
 minuten:
-; mov r6, #3bh
-; dec r7
 call zeigen
 ret
 sekunden:
-; inc r6
 call zeigen
 ret
 ;-------------------------------------------------------
-; Anzeigewerte: holt die Anzeigewerte aus der Datenbank
-; - erst wird aus dem Hex_Wert ein Dezimalwert : BCD-Umrechnung
-; dann wird der Wert mit @A+DPTR aus der Datenbank 
-; in die Register geschrieben
-; 1er Sekunden => P3=R2
-; 10er Sekunden => P3=R3
-; 1er Minuten  => P3=R4
-; 10er Minuten  => P3=R5
+; zeigen
 ;-------------------------------------------------------
 zeigen:
 mov DPTR, #table
@@ -135,48 +133,67 @@ mov a, R6
 mov b, #0ah
 div ab
 mov R0, a
-movc a,@a+dptr
+movc a, @a+dptr
 mov r3, a
 mov a, r0
-xch a,b
+xch a, b
 movc a, @a+dptr
 mov r2, a
-;----------------
 mov a, R7
 mov b, #0ah
 div ab
 mov R0, a
-movc a,@a+dptr
+movc a, @a+dptr
 mov r5, a
 mov a, r0
-xch a,b
+xch a, b
+movc a, @a+dptr
+mov r4, a
+call display
+ret
+;-----------------------------------------------zeigen:
+mov DPTR, #table
+mov a, R6
+mov b, #0ah
+div ab
+mov R0, a
+movc a, @a+dptr
+mov r3, a
+mov a, r0
+xch a, b
+movc a, @a+dptr
+mov r2, a
+mov a, R7
+mov b, #0ah
+div ab
+mov R0, a
+movc a, @a+dptr
+mov r5, a
+mov a, r0
+xch a, b
 movc a, @a+dptr
 mov r4, a
 call display
 ret
 ;-----------------------------------------------
-;   DISPLAY: steuert die 4x7 Segmentanzeige
+; display
 ;-----------------------------------------------
 display:
 mov P3, R2
 clr P2.0
 setb P2.0
-
 mov P3, R3
 clr P2.1
 setb P2.1
-
 mov P3, R4
 clr P2.2
 setb P2.2
-
 mov P3, R5
 clr P2.3
 setb P2.3
-
 ret
 ;-------------------------------------------------
-; TABLE: Datenbank der 7-Segment-Darstellung
+; TABLE
 ;-------------------------------------------------
 org 300h
 table:
@@ -184,5 +201,31 @@ db 11000000b
 db 11111001b, 10100100b, 10110000b
 db 10011001b, 10010010b, 10000010b
 db 11111000b, 10000000b, 10010000b
+end
 
+; display
+;-----------------------------------------------
+display:
+mov P3, R2
+clr P2.0
+setb P2.0
+mov P3, R3
+clr P2.1
+setb P2.1
+mov P3, R4
+clr P2.2
+setb P2.2
+mov P3, R5
+clr P2.3
+setb P2.3
+ret
+;-------------------------------------------------
+; TABLE
+;-------------------------------------------------
+org 300h
+table:
+db 11000000b
+db 11111001b, 10100100b, 10110000b
+db 10011001b, 10010010b, 10000010b
+db 11111000b, 10000000b, 10010000b
 end
